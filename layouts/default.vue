9b9a00e5d37a1a6bd6d0fab49599bb5b81d00f88
akatsuka <akasuke-419tsuk@docomo.ne.jp>
2020-05-17 22:59:58 +0900

    Initial Commit

---
+++ layouts/default.vue
@@ -0,0 +1,194 @@
+<template>
+  <v-app dark>
+
+    <link href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
+
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+      class="black"
+    >
+      <img 
+        src="../static/logo2020-dark-toka.png" 
+        width="40" 
+        height="40" 
+        @click="$vuetify.goTo(0)"
+      >
+      <v-spacer/>
+      <v-toolbar-title 
+        @click="$vuetify.goTo(0)" 
+        v-text="title" 
+        class="font-weight-medium"
+      />
+      <v-spacer/>
+      <v-btn icon href="https://twitter.com/apmayfes?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">
+        <v-icon>fab fa-twitter</v-icon>
+      </v-btn>
+    </v-app-bar>
+    
+    <v-content>
+
+      <v-img
+       class="hidden-xs-only"
+       height="620px"
+      :src="require('../static/chirashi-yokonaga4.png')"
+      >
+        <v-row 
+          align="end" 
+          class="lightbox pa-2 fill-height" 
+          justify="center"
+        >
+          <v-col cols="12" lg="11" md="11">
+            <v-card class="mx-4">
+              <v-card-title class="font-weight-bold text-center justify-center pt-4 display-1 font-weight-medium heading">
+                  工学博覧会ONLINE 開催予定
+              </v-card-title>
+              <v-card-text class="text-center text--primary body-1">
+                詳しくはこちら　
+                <span
+                style="text-decoration: underline" 
+                @click="$vuetify.goTo('#covid-19')"
+                >
+                COVID-19に関する対応について
+                </span>
+              </v-card-text>
+              <v-tabs
+                v-model="tab"
+                color="#B4186E"
+                grow
+              >
+                <v-tab
+                  v-for="(item,i) in items"
+                  :key="i"
+                  class="font-weight-bold white--text"
+                >
+                  {{ item.title }}
+                </v-tab>
+              </v-tabs>
+              <v-tabs-items v-model="tab">
+                <v-tab-item
+                  v-for="item in items"
+                  :key="item"
+                >
+                  <v-card flat>
+                    <v-card-text class="text--primary">
+                      {{ item.text1 }}<br>
+                      {{ item.text2 }}<br>
+                      {{ item.text3 }}
+                    </v-card-text>
+                  </v-card>
+                </v-tab-item>
+              </v-tabs-items>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-img>
+
+      <v-img 
+       class="hidden-sm-and-up"
+      :src="require('../static/chirashi_sns.png')"
+      >
+        <v-row 
+          align="end" 
+          class="lightbox pa-2 fill-height" 
+          justify="center"
+        >
+          <v-col cols="12" lg="11" md="11">
+            <v-card class="mx-4">
+              <v-card-title class="font-weight-bold text-center justify-center py-4 headline heading">
+                  工学博覧会ONLINE 開催予定
+              </v-card-title>
+              <v-card-text class="text-center text--primary body-1">
+                <span
+                style="text-decoration: underline" 
+                @click="$vuetify.goTo('#covid-19')"
+                >
+                COVID-19に関する対応について
+                </span>
+              </v-card-text>
+              <v-expansion-panels accordion>
+                <v-expansion-panel
+                  v-for="(item,i) in items"
+                  :key="i"
+                >
+                  <v-expansion-panel-header>
+                    {{ item.title }}
+                    <template v-slot:actions>
+                      <v-icon color="#B4186E">$expand</v-icon>
+                    </template>
+                  </v-expansion-panel-header>
+                  <v-expansion-panel-content class="caption">
+                    {{ item.text1 }}<br>
+                    {{ item.text2 }}<br>
+                    {{ item.text3 }}
+                  </v-expansion-panel-content>
+                </v-expansion-panel>
+              </v-expansion-panels>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-img>
+
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer class="black">&copy; 2020東大工学部応用物理系学科有志</v-footer>
+  </v-app>
+</template>
+
+<style>
+*{
+  font-family: 
+    "Helvetica Neue",
+    Arial,
+    "Hiragino Kaku Gothic ProN",
+    "Hiragino Sans",
+    sans-serif;
+}
+
+.heading{
+  color: #FFCCFF;
+}
+</style>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      tab: null,
+      title: '工学博覧会2020',
+      items: [
+        {
+          title:'工学博覧会とは',
+          text1:'工学博覧会は物理工学科と計数工学科の学生有志が、日頃学んでいる学問や学生生活を紹介する展示企画です。皆さんが中学や高校で学ぶ数学や物理は工学に用いられ、生活の役に立ったり時には人を感動させる作品を作り上げます。',
+          text2:'見て、触って、話して、美しき工学の世界を一緒に楽しみませんか？',
+          text3:''
+        },
+        {
+          title:'代表者あいさつ',
+          text1:'工学博覧会のホームページを訪れてくださり、ありがとうございます。今年は皆様を『応物の世界に引き込む』ことをテーマに、去年までの展示デザインを刷新し、より学科の魅力の詰まった企画を学生一丸となって準備しております。新しく生まれ変わった工学博覧会で皆様とお会いできることを学生一同楽しみにお待ちしております。',
+          text2:'',
+          text3:'工学博覧会代表　藤田尚輝'
+        },
+        {
+          title:'今年度の展示コンセプト',
+          text1:'ーーーーようこそ、応物の世界へーーーー',
+          text2:'今年の工学博覧会では「工学」と「絵本」の融合した新たな学科展示のカタチを作りました。絵本のタイトルは『応物の国のメイプ』。この絵本の主人公、メイプちゃんと共にあらゆる工学の集まった応物の世界であなたは不思議な体験をすることになるでしょう。',
+          text3:'さあ、１ページ目を開いてみましょう。'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+
---
+++ pages/index.vue
@@ -0,0 +1,211 @@
+<template>
+  <v-content class="py-0">
+    <v-container class="py-0"> 
+
+      <h2>新着情報</h2>
+        <v-card
+          class="overflow-y-auto my-6 px-4 py-2" 
+          height="200"
+        >
+          <div
+            v-for="(item,i) in news"
+            :key="i"
+          >
+            <span class="caption font-weight-light">{{ item.date }}</span><br>
+            {{ item.text }}
+            <v-divider class="divider my-2"></v-divider>
+          </div>
+        </v-card>
+
+      <h2 id="covid-19">COVID-19に関する対応について</h2>
+      <p class="ma-4">COVID-19の感染拡大に伴い五月祭が当初日程での開催を断念したことを受け、工学博覧会は以下の方針で開催準備を進めています。</p>
+      <h3>工学博覧会ONLINEの開催(10月中を予定)</h3>
+      <p class="ma-4">参加者間で今後の方針について検討を行った結果、本HPやYouTube Live、Clusterなどを活用したオンライン上での開催を目標とすることに決定いたしました。現在、１０月中の開催に向け準備を進めています。日程が近づきましたら本HPおよびTwitterなどで詳細をお知らせします。</p>
+      <h3>五月祭への参加(開催が確定した場合)</h3>
+      <p class="ma-4">五月祭の延期開催が決定した場合は、上記のオンライン開催と併せて五月祭への参加も予定しております。</p>
+
+      <h2 class="mt-6">企画紹介</h2>
+      <h3 class="mt-4">実験班</h3>
+      <p class="ma-4">計数工学科・物理工学科の学生達が、それぞれの興味関心に応じて制作した実験を展示しています。</p>
+      <v-row>
+        <template v-for="(item, i) in items_1">
+          <v-col
+            :key="i"
+            cols="12"
+            sm="6"
+            lg="4"
+          >
+            <v-hover v-slot:default="{ hover }">
+              <v-card
+                :elevation="hover ? 12 : 2"
+                :class="{ 'on-hover': hover }"
+                class="my-4"
+                color="black"
+              >
+                <v-img :src="item.img" height="200px"></v-img>
+                <div class="kikaku_parent">
+                  <img :src="item.icon" width="40" height="40" class="kikaku_child">
+                  <h4 class="kikaku_child">{{ item.title }}</h4>
+                </div>
+              </v-card>
+            </v-hover>
+          </v-col>
+        </template>
+      </v-row>    
+      <h3 class="mt-4">企画班</h3>
+      <p class="ma-4 headline">Coming soon…</p>
+      
+      
+
+      <h2 class="mt-6">アクセス</h2>
+      <v-row>
+        <v-col cols="12" lg="8">
+          <v-img :src="require('../static/campasmap-1.jpg')"></v-img>
+        </v-col>
+        <v-col cols="12" lg="4">
+          <v-card class="px-4 py-1" height="100%">
+            <h4 class="pb-2 my-4 border-bottom">会場</h4>
+            <p>東京大学 本郷キャンパス工学部6号館</p>
+            <h4 class="pb-2 my-4 border-bottom">最寄駅</h4>
+            <p>東大前駅(南北線)　徒歩8分</p>
+            <p>本郷三丁目駅(丸の内線)　徒歩12分</p>
+            <p>根津駅(千代田線)　徒歩12分</p>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-divider class="my-6"></v-divider>
+
+      <p class="text-center headline font-weight-bold"><span class="rink">リンク</span></p>
+      <v-row justify="center">
+        <template v-for="(item, i) in items_3">
+          <v-col
+            :key="i"
+            cols="12"
+            sm="6"
+            md="4"
+            align="center"
+          >
+            <a v-bind:href="item.url">
+              <img class="ma-3" :src="item.img">
+            </a>
+          </v-col>
+        </template>
+      </v-row>
+
+    </v-container> 
+  </v-content>
+</template>
+
+<style>
+h2{
+  border-left: solid 4px #008080;
+  border-bottom: solid 1px #008080;
+  padding: 5px 10px;
+}
+
+h3{
+  border-left: solid 3px #B4186E;
+  padding: 0 10px;
+}
+
+.divider{
+  background-color: #B4186E;
+}
+
+.kikaku_parent{
+  height: 45px;
+  line-height: 45px;
+  padding-left: 10px;
+}
+
+.kikaku_child{
+  display: inline-block;
+  vertical-align: middle;
+}
+
+.border-bottom{
+  border-bottom: solid 1px #B4186E;
+}
+
+.rink{
+  border-bottom: solid 2px #B4186E;
+}
+
+img:hover {
+	opacity: 0.7;
+}
+</style>
+
+<script>
+  export default {
+    data: () => ({
+      tab:null,
+      items_1: [
+        {
+          title: '脳科学班',
+          img: require('../static/noukagaku.jpg'),
+          icon: require('../static/logo-brain-touka.png')
+        },
+        {
+          title: 'コンピューターの頭脳班',
+          img: require('../static/cpu.JPG'),
+          icon: require('../static/logo-cpu-touka.png')
+        },
+        {
+          title: 'vision班',
+          img: require('../static/vision.png'),
+          icon: require('../static/logo-vision-touka.png')
+        },
+        {
+          title: '芸術表現班',
+          img: require('../static/art2.jpg'),
+          icon: require('../static/logo-art-v2-touka.png')
+        },
+        {
+          title: '光班',
+          img: require('../static/hikari.jpeg'),
+          icon: require('../static/logo-hikari-touka.png')
+        },
+        {
+          title: '物性班',
+          img: require('../static/bussei.jpg'),
+          icon: require('../static/logo-bussei-touka.png')
+        }
+      ],
+      
+      items_3: [
+        {
+          img: require('../static/ba-na-_butuko.png'),
+          url: "http://www.ap.t.u-tokyo.ac.jp/"
+        },
+        {
+          img: require('../static/ba-na-_keisu.png'),
+          url: "https://www.keisu.t.u-tokyo.ac.jp/"
+        },
+        {
+          img: require('../static/ba-na-_iinkai.png'),
+          url: "https://gogatsusai.jp/93/visitor/"
+        }
+
+      ],
+      news: [
+        {
+          date: '2020-5-16',
+          text: '工学博覧会2020HPを公開しました。'
+        },
+        {
+          date: '2020-5−17',
+          text: '工学博覧会2020HPを公開しました'
+        },
+        {
+          date: '2020-5-18',
+          text: '工学博覧会2020HPを公開しました'
+        },
+        {
+          date: '2020-5-19',
+          text: '工学博覧会2020HPを公開しました'
+        }, 
+      ]
+    })
+  }
+</script>
