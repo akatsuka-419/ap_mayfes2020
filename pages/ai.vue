cb6bd338a4580d39680b3d350799b35c4819bd52
akatsuka <akasuke-419tsuk@docomo.ne.jp>
2020-09-20 03:50:40 +0900

    fix

--- pages/ai.vue
+++ pages/ai.vue
@@ -60,9 +60,9 @@
                     <p class="mt-6 subtitle-2">詳しい解説はこちら</p>
 
                     
-                    <nuxt-link to="../PDF/機械学習班_1_顔のトラッキング.pdf">
+                    <a href="/機械学習班_1_顔のトラッキング.pdf">
                     <img src="../assets/img/PDF_tmb/機械学習班_1_顔追跡ゲーム.png" ontouchstart="" class="img_rink mr-4">
-                    </nuxt-link>
+                    </a>
                     
                     <a href="../assets/PDF/機械学習班_1_顔のトラッキング.pdf" target="_blank" rel="noopener">
                     <img src="../assets/img/PDF_tmb/機械学習班_1_顔のトラッキング.png" ontouchstart="" class="img_rink">
